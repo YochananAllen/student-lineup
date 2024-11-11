938de8c19fd64ab8953fbb06a71d2579fbae6e9b
Yochanan allen <107517904+YochananAllen@users.noreply.github.com>
2024-11-11 15:28:26 -0500

    Update student lineup.cpp

--- student lineup.cpp
+++ student lineup.cpp
@@ -1,5 +1,11 @@
 // student lineup.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //Name: Yochanan Allen 
+//A teacher has asked all her students to line up according to their first name.
+//For example, in one class Amy will be at the front of the line, and Yolanda will be at the end.
+//Write a program that prompts the user to enter the number of students in the class, then loops to read that many names.
+//Once all the names have been read, it reports which student would be at the front of the line and
+//which one would be at the end of the line. You may assume that no two students have the same name.
+//Input Validation: Do not accept a number less than 1 or greater than 25 for the number of students.
 
 #include <iostream>
 #include <string>
